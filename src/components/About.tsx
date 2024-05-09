ef1b96dd3a269e3268e55100e88bd19496e7882f
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2024-05-10 01:09:55 +0600

    updated bio info and style change

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ function About() {
       id="about"
       className="mb-16 scroll-mt-16 mg:mb-24 lg:scroll-mt-24 lg:mb-28"
     >
-      <h2 className="font-medium mb-5 lg:hidden tracking-wider">ABOUT</h2>
+      <h2 className="font-medium mb-6 lg:hidden tracking-wider">ABOUT</h2>
       <div className="flex flex-col gap-4 leading-[1.6]">
         <p>Hi there! 👋 I'm Sabbir. </p>
         <p>
@@ -17,26 +17,16 @@ function About() {
           >
             ReliSource
           </a>{" "}
-          in Bangladesh. I deal with things like building the backbones of web
-          applications, managing data in the cloud, and making websites look
-          good and work smoothly.
+          in Bangladesh. I deal with things like building the backbones of web applications, managing data in the cloud, and making websites look good and work smoothly.
         </p>
         <p>
-          During my software engineering career, I have worked with multiple
-          programming languages, libraries, frameworks, and technologies such as
-          Ruby on Rails, Apex, TypeScript, React, Postgres, AngularJS, and AWS.
+          During my software engineering career, I have worked with multiple programming languages, libraries, frameworks, and technologies such as Ruby on Rails, Apex, TypeScript, React, Postgres, AngularJS, and AWS.
         </p>
         <p>
-          My interests range from distributed systems and big data to AI. I'm
-          constantly driven to push my boundaries and explore new technological
-          horizons.
+          My interests range from distributed systems and big data to AI. I'm constantly driven to push my boundaries and explore new technological horizons. 🚀
         </p>
         <p>
-          Outside of work, I'm deeply committed to personal growth and
-          self-improvement. I firmly believe in the power of continuous
-          learning, both professionally and personally. In my downtime, you will
-          find me lost in the pages of a captivating book 📚 or listening to
-          podcasts 🎙️
+          Outside of work, I'm deeply committed to personal growth and self-improvement. I firmly believe in the power of continuous learning, both professionally and personally. In my downtime, you will find me lost in the pages of a captivating book 📚 or listening to podcasts 🎙️
         </p>
       </div>
     </section>
