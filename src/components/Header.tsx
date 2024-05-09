0c76483aadec467354fb4e85ec29e541a571072b
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2024-05-10 01:09:28 +0600

    added socials to hero icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,10 @@ function Header() {
   return (
     <header className="lg:w-1/2 lg:sticky lg:top-0 lg:max-h-screen lg:py-24 lg:flex lg:flex-col lg:justify-between">
       <div className="flex flex-col">
-        <h1 className="text-4xl font-bold font-poppins sm:text-5xl tracking-tight">
+        <h1 className="text-4xl font-bold sm:text-5xl tracking-tighter">
           Sabbir Ahmed
         </h1>
-        <h2 className="mt-3 text-xl font-medium tracking-tight sm:text-xl">
+        <h2 className="mt-3 text-xl font-medium sm:text-xl">
           Software Engineer
         </h2>
         <p className="mt-4 leading-normal">I build things for web</p>
@@ -21,10 +21,34 @@ function Header() {
       </nav>
 
       <footer className="mt-8 flex gap-5 items-center">
-        <IconGithub className="size-6" />
-        <IconLinkedin className="size-6" />
-        <IconInstagram className="size-6" />
-        <IconX className="size-6" />
+        <a
+          href="https://github.com/sabbir256"
+          target="_blank"
+          className="opacity-90 hover:opacity-100"
+        >
+          <IconGithub className="size-6" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/sabbir96/"
+          target="_blank"
+          className="opacity-90 hover:opacity-100"
+        >
+          <IconLinkedin className="size-6" />
+        </a>
+        <a
+          href="https://www.instagram.com/isabbirahmed_/"
+          target="_blank"
+          className="opacity-90 hover:opacity-100"
+        >
+          <IconInstagram className="size-6" />
+        </a>
+        <a
+          href="https://x.com/mesabbir_"
+          target="_blank"
+          className="opacity-90 hover:opacity-100"
+        >
+          <IconX className="size-6" />
+        </a>
       </footer>
     </header>
   );
